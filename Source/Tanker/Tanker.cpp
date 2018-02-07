2628411a1b978be944eb8a9a94f303b003ba9564
Marko <marko.simunovic89@gmail.com>
2018-02-07 15:40:34 +0100

    Initial commit

---
+++ Source/Tanker/Tanker.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tanker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tanker, "Tanker" );
